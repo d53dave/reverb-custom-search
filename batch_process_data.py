134ffc74eeb20a8ce21e0c2573d6451c8485ff2b
David Sere <dave@d53dev.net>
2019-11-21 10:01:56 +0100

    Add scheduling with APScheduler, add bot integration

--- batch_process_data.py
+++ batch_process_data.py
@@ -4,8 +4,16 @@ import requests
 import json
 import time
 import arrow
+import logging
 
 from typing import List, Tuple, Dict, Any
+from collections import defaultdict
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+from telegram_bot import Bot
+
+logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',
+                    level=logging.INFO)
 
 REVERB_LISTING_API_URL = 'https://api.reverb.com/api/listings'
 REVERB_API_CODE = os.getenv('REVERB_API_CODE')
@@ -16,7 +24,8 @@ PG_HOST = os.getenv('PG_HOST')
 PG_DB = os.getenv('PG_DB')
 
 if REVERB_API_CODE is None:
-    print('Cannot run batch import listing: no REVERB_API_KEY env found')
+    logging.error(
+        'Cannot run batch import listing: no REVERB_API_KEY env found')
     exit(1)
 
 req_headers = {
@@ -29,10 +38,20 @@ insert_query = """
     INSERT INTO reverb_guitar_data.listing (id, make, model, year, condition, price_cents, currency, offers_enabled, thumbnail, full_json) VALUES %s
 """
 
+conn = psycopg2.connect(
+    f'dbname={PG_DB} user={PG_USER} password={PG_PASS} port={PG_PORT} host={PG_HOST}'
+)
+conn.autocommit = True
+cur = conn.cursor()
+
+bot = Bot(str(os.getenv('TELEGRAM_TOKEN')))
+
+sched = BlockingScheduler()
+
 
 class Listing():
     def __init__(self, values):
-        self.full_json: str = json.dumps(values)
+        self.full_json: Dict = values
 
         self.id: int = int(values['id'])
         self.make: str = values['make']
@@ -50,7 +69,7 @@ class Listing():
 def get_listings_with_query(query: Dict[str, Any]
                             ) -> Tuple[List[Listing], str]:
 
-    print('Getting listings with params:', query)
+    logging.info('Getting listings with params: %s', query)
     response = requests.get(REVERB_LISTING_API_URL,
                             params=query,
                             headers=req_headers)
@@ -66,16 +85,18 @@ def get_listings_for_url(url) -> Tuple[List[Listing], str]:
     resp_json = response.json()
 
     if 'Used Electric Guitars' not in resp_json['humanized_params']:
-        print('WARN: `humanized_params` looks wonky - ' +
-              resp_json['humanized_params'])
+        logging.warn('`humanized_params` looks wonky - %s',
+                     resp_json['humanized_params'])
 
     return list(
         map(lambda x: Listing(x),
             resp_json['listings'])), resp_json['_links']['next']['href']
 
 
-def insert_listings(cur, listings: List[Listing]) -> int:
-    duplicates = 0
+def insert_listings(cur, listings: List[Listing]
+                    ) -> Tuple[List[Listing], List[Listing]]:
+    duplicates = []
+    inserted = []
     for listing in listings:
         try:
             cur.execute(
@@ -83,24 +104,41 @@ def insert_listings(cur, listings: List[Listing]) -> int:
                 (listing.id, listing.make, listing.model, listing.year,
                  listing.condition, listing.price_cents, listing.currency,
                  listing.offers_enabled, listing.created_at.datetime,
-                 listing.thumbnail, listing.full_json))
+                 listing.thumbnail, json.dumps(listing.full_json)))
+            inserted.append(listing)
         except psycopg2.IntegrityError as e:  # 23505 is constraint unique
             if e.pgcode == '23505':
-                duplicates += 1
+                duplicates.append(listing)
                 continue
             else:
                 raise e
-    return duplicates
+    return inserted, duplicates
 
 
-if __name__ == '__main__':
-    print(arrow.utcnow(), 'Running batch reverb update')
-    conn = psycopg2.connect(
-        f'dbname={PG_DB} user={PG_USER} password={PG_PASS} port={PG_PORT} host={PG_HOST}'
-    )
-    conn.autocommit = True
-    cur = conn.cursor()
+def process_bot_notifications(bot: Bot, listings: List[Listing]) -> None:
+    bot_terms = bot.get_terms()
+    all_results: Dict[str, List[Listing]] = defaultdict(list)
+    for listing in listings:
+        for term in bot_terms:
+            lower_term = term.lower()
+            if lower_term in listing.full_json['title'].lower(
+            ) or lower_term in listing.model.lower(
+            ) or lower_term in listing.make.lower(
+            ) or lower_term in listing.full_json['description'].lower():
+                all_results[term].append(listing)
 
+    for term, listings in all_results.items():
+        strings = list(
+            map(
+                lambda lst:
+                f"[{lst.full_json['title']}]({lst.full_json['_links']['web']['href']})",
+                listings))
+        bot.send_update(term, strings)
+
+
+@sched.scheduled_job('cron', minute='*/7')
+def update_listings():
+    logging.info('Running batch reverb update')
     query = {
         'page': 1,
         'per_page': 50,
@@ -110,21 +148,35 @@ if __name__ == '__main__':
     }
 
     listings, next_url = get_listings_with_query(query)
-    print('Processing first page')
-    duplicates = insert_listings(cur, listings)
-    if duplicates > 0:
-        print('Found duplicates:', duplicates)
-    if duplicates == 50:
-        print('Exiting: Found full page of duplicates')
-        exit(0)
+
+    logging.info('Processing first page')
+    inserted, duplicates = insert_listings(cur, listings)
+
+    process_bot_notifications(bot, inserted)
+
+    if len(duplicates) > 0:
+        logging.info('Found duplicates: %d', len(duplicates))
+    if len(inserted) == 0:
+        logging.info('Exiting: Found full page of duplicates')
+        return
 
     while next_url is not None:
-        time.sleep(15)
-        print('Processing url', next_url)
+        time.sleep(10)
+        logging.info('Processing url %s', next_url)
         listings, next_url = get_listings_for_url(next_url)
-        duplicates = insert_listings(cur, listings)
-        if duplicates > 0:
-            print('Found duplicates:', duplicates)
-        if duplicates == 50:
-            print('Exiting: Found full page of duplicates')
-            exit(0)
+        inserted, duplicates = insert_listings(cur, listings)
+
+        process_bot_notifications(bot, inserted)
+
+        if len(duplicates) > 0:
+            logging.info('Found duplicates: %d', len(duplicates))
+        if len(inserted) == 0:
+            logging.info('Exiting: Found full page of duplicates')
+            return
+
+
+if __name__ == '__main__':
+    try:
+        sched.start()
+    except KeyboardInterrupt:
+        logging.info('Got SIGTERM! Terminating...')
